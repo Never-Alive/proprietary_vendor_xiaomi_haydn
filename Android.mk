e67be457786cff21f7b5f6507459e06bec0ef15e
Never-Alive <livinglucifer0@gmail.com>
2026-09-06 07:59:34 +0000

    haydn: Drop firmware
    Signed-off-by: Never-Alive <livinglucifer0@gmail.com>

--- Android.mk
+++ Android.mk
@@ -3,25 +3,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),haydn)
-
-$(call add-radio-file-sha1-checked,radio/abl.img,c000363ceb845c339646ed1a2bfd912e2bfcb143)
-$(call add-radio-file-sha1-checked,radio/aop.img,36535362f706d6425fb2fa56e0a832b85e354c71)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,64b6eb270a8db8f587b0dc7a95518895f12f5aea)
-$(call add-radio-file-sha1-checked,radio/cpucp.img,63568581afe5ad0b17f9741eb1132da082a677bc)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,4956fafcd5c5eb11c449616ce7e3e0a6b618833e)
-$(call add-radio-file-sha1-checked,radio/dsp.img,453413b40e59bfbd20bcb03dce4e3538f0f04550)
-$(call add-radio-file-sha1-checked,radio/featenabler.img,afba3c1914c1ad4d11e8711b0ee7bdfcdc46e4bb)
-$(call add-radio-file-sha1-checked,radio/hyp.img,b81d58d017193ede2eb4992c92ae3c0772464804)
-$(call add-radio-file-sha1-checked,radio/imagefv.img,cd6b405ccac493ab98e36b98a6826c827f11818e)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,c83435f329cf1c6b020d4e9875f9312c4209104c)
-$(call add-radio-file-sha1-checked,radio/modem.img,ea3412d3fd3f5089df03030bc741791b746d030b)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,fc2607154e0c03a66387263aacf95610dfaaa370)
-$(call add-radio-file-sha1-checked,radio/shrm.img,9845c59a3d56ab71730ac4069029b9707d01aadd)
-$(call add-radio-file-sha1-checked,radio/tz.img,b195d8a7164dd54daadf00caead35c81a17cc112)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,f3dba3897fb82905ae941c01036925375e8a46e6)
-$(call add-radio-file-sha1-checked,radio/xbl.img,73c56df97c238f79e461984af8785c4ce16c9bb5)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,55017358733f37d0ab160ccc16c1a96001c89131)
-
-endif
